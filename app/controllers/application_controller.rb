565beda074d02879f95d798dcfdaa0e0863dea85
tech-kannnari <madpowertk@gmail.com>
2021-02-26 19:29:20 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # ENV[変数]←同じものがある場合上から順に適応されるので、変数名を変えるか削除する必要がある
+      username == ENV["BASIC_AUTH_USERS"] && password == ENV["BASIC_AUTH_PASSWORDS"]
+    end
+  end
+
 end
