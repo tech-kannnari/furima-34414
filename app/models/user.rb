f0945e32acb30ea54c8a1d19d9b79351777748fb
tech-kannnari <madpowertk@gmail.com>
2021-02-27 18:19:45 +0900

    userテーブルの設計、カラムのバリデーション

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  
+  validates :nickname, presence:true
+  validates :last_name, presence:true
+  validates :name, presence:true
+  validates :last_kana, presence:true
+  validates :kana, presence:true
+  validates :birthday, presence:true
+end
