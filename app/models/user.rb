3c595acc4851539227a7e63218cb8d3033717a82
tech-kannnari <madpowertk@gmail.com>
2021-02-27 18:46:35 +0900

    emailのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  validates :email, uniqueness: true
   validates :nickname, presence:true
   validates :last_name, presence:true
   validates :name, presence:true
