01ae298a5b7d86e4a00fbe17efeb5b977be9a84c
tech-kannnari <madpowertk@gmail.com>
2021-03-01 21:16:41 +0900

    last_name,name,last_kana,kanaのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,31 @@ class User < ApplicationRecord
   has_many :items
   has_many :histories
 
-  validates :email, uniqueness: true
+  # validates :email, uniqueness: true
   validates :nickname, presence:true
-  validates :last_name, presence:true
-  validates :name, presence:true
-  validates :last_kana, presence:true
-  validates :kana, presence:true
+
+  validates :last_name, presence:true, format: {
+    with: /\A[ぁ-んァ-ン一-龥]/,
+    message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい"
+  }
+  validates :name, presence:true, format: {
+    with: /\A[ぁ-んァ-ン一-龥]/,
+    message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい"
+  }
+  validates :last_kana, presence:true, 
+  format: {
+    with: /\A[ァ-ヶー－]+\z/,
+    message: "全角カタカナのみで入力して下さい"
+  }
+  validates :kana, presence:true, 
+  format: {
+    with: /\A[ァ-ヶー－]+\z/,
+    message: "全角カタカナのみで入力して下さい"
+  }
   validates :birthday, presence:true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
 end
+
