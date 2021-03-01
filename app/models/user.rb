256a1e628f304ac8a781f4aa3a2da4b223083fc5
tech-kannnari <madpowertk@gmail.com>
2021-03-01 12:48:46 +0900

    userモデルのアソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  has_many :histories
+
   validates :email, uniqueness: true
   validates :nickname, presence:true
   validates :last_name, presence:true
