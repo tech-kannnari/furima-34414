16750783d50c7d669ceee0fb728385ba50b1c52a
tech-kannnari <madpowertk@gmail.com>
2021-03-02 11:25:15 +0900

    with_optionsの使用、正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,31 +7,34 @@ class User < ApplicationRecord
   has_many :items
   has_many :histories
 
-  # validates :email, uniqueness: true
-  validates :nickname, presence:true
+  # validates :email, uniqueness: true ← こちらはuserのmigrateファイル内にデフォルトでバリデイトされているので記述はいらない
+  with_options presence: true do
 
-  validates :last_name, presence:true, format: {
-    with: /\A[ぁ-んァ-ン一-龥]/,
-    message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい"
-  }
-  validates :name, presence:true, format: {
-    with: /\A[ぁ-んァ-ン一-龥]/,
-    message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい"
-  }
-  validates :last_kana, presence:true, 
-  format: {
+    validates :nickname
+    
+    validates :last_name, format: {
+      with: /\A[ぁ-んァ-ン一-龥々]+\z/,
+      message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい"
+    }
+    validates :name,format: {
+      with: /\A[ぁ-んァ-ン一-龥々]+\z/,
+      message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい"
+    }
+    validates :last_kana, format: {
     with: /\A[ァ-ヶー－]+\z/,
     message: "全角カタカナのみで入力して下さい"
-  }
-  validates :kana, presence:true, 
-  format: {
+    }
+    validates :kana, format: {
     with: /\A[ァ-ヶー－]+\z/,
     message: "全角カタカナのみで入力して下さい"
-  }
-  validates :birthday, presence:true
-  
+    }
+    validates :birthday
+  end
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
 end
+
+
 
