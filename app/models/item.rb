5eee790efd12756229ad0f8b34677e7841574a55
tech-kannnari <madpowertk@gmail.com>
2021-03-02 18:10:39 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,16 @@ class Item < ApplicationRecord
   
   belongs_to :user
 
-  has_one_attached :image
+  with_options presence: true do
+    validates :image, has_one_attached: 
+    validates :name
+    validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :delivery_fee_id
+    validates :delivery_area_id
+    validates :delivery_day_id 
+    validates :price
+  end
 
 end
