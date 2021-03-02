17aa69e46145303b1e23de19a41e7f84a3e5b562
tech-kannnari <madpowertk@gmail.com>
2021-03-02 15:49:15 +0900

    emailの一意性に関するテストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,19 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it 'emailに@を含まないと登録できない' do
+        @user.email = 'tttooo'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'emailは同じものは登録できない' do
+        binding.pry
+         @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
