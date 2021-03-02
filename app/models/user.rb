d1a55490472e7e62ed9938c155d75994f6bf4e78
tech-kannnari <madpowertk@gmail.com>
2021-03-02 16:32:14 +0900

    with_optionsでの記述の省略

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,35 +8,30 @@ class User < ApplicationRecord
 
   # validates :email, uniqueness: true ← こちらはuserのmigrateファイル内にデフォルトでバリデイトされているので記述はいらない
   with_options presence: true do
-
-    validates :nickname
     
-    validates :last_name, format: {
-      with: /\A[ぁ-んァ-ン一-龥々]+\z/,
-      message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい"
-    }
-    validates :name,format: {
-      with: /\A[ぁ-んァ-ン一-龥々]+\z/,
-      message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい"
-    }
-    validates :last_kana, format: {
-    with: /\A[ァ-ヶー－]+\z/,
-    message: "全角カタカナのみで入力して下さい"
-    }
-    validates :kana, format: {
-    with: /\A[ァ-ヶー－]+\z/,
-    message: "全角カタカナのみで入力して下さい"
-    }
+    validates :nickname
     validates :birthday
-
+    
+    with_options format: {
+      with: /\A[ぁ-んァ-ン一-龥々]+\z/,
+      message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" } do
+      validates :last_name
+      validates :name
+    end
+    
+    with_options format: {
+      with: /\A[ァ-ヶー－]+\z/,
+      message: "全角カタカナのみで入力して下さい" } do
+      validates :last_kana 
+      validates :kana
+    end
+    
     validates :password, 
       format: {
       with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,20}\z/,
       message: "には半角英字と数字の両方を含めて6文字以上で設定してください"
       }
   end
-
-
 end
 
 
