710b711e48a861f576cc2677a2d0eb0f2f88fe6c
tech-kannnari <madpowertk@gmail.com>
2021-03-02 18:23:59 +0900

    new,createアクションとそのルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,11 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # アクセスがあったら"items#index"へルーティングされますよ
   root to: "items#index"
+  resources :items, only:[:new, :create]
 
 end
 
