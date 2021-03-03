3202cc864758595da48373cf63abeaf645a4f297
tech-kannnari <madpowertk@gmail.com>
2021-03-03 19:28:59 +0900

    itemモデルのfactory botの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
+
   factory :item do
-    
+      name              {Faker::Name}
+      description       {Faker::Lorem}
+      category_id       {2}
+      item_status_id    {2}
+      delivery_fee_id   {2}
+      delivery_area_id  {"2"}
+      delivery_day_id   {"2"}
+      price             {10000}
+      association :user
+
+      after(:build) do |item|
+        item.image.attach(io: File.open('public/images/omuraisu.png'), filename: 'omuraisu.png')
+      end
   end
 end
