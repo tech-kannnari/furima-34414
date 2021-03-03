b325078e9c7a0b3d34f1fa635dc656bf39af4950
tech-kannnari <madpowertk@gmail.com>
2021-03-03 12:02:35 +0900

    アクティブハッシュの導入とアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,29 @@ class Item < ApplicationRecord
   
   belongs_to :user
 
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :item_status
+    belongs_to :delivery_fee
+    belongs_to :delivery_area
+    belongs_to :delivery_day 
+
   with_options presence: true do
-    validates :image, has_one_attached: 
     validates :name
     validates :description
-    validates :category_id
-    validates :item_status_id
-    validates :delivery_fee_id
-    validates :delivery_area_id
-    validates :delivery_day_id 
     validates :price
-  end
 
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :item_status_id
+      validates :delivery_fee_id
+      validates :delivery_area_id
+      validates :delivery_day_id 
+    end
+  end
 end
+
+
+
