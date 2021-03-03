8973b220e877427808a17c09bf12f006c40353a7
tech-kannnari <madpowertk@gmail.com>
2021-03-03 12:06:32 +0900

    index,new,createアクション内の記述追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,26 @@
 class ItemsController < ApplicationController
   
   def index
+    @items = item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :item_status_id, 
-      :delivery_fee_id, :delivery_area_id, :delivery_day_id, :price, :user)
+    params.require(:item).permit(:name, :description, :category_id, :item_status_id, :delivery_fee_id, 
+      :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id,) 
   end
 
 end
