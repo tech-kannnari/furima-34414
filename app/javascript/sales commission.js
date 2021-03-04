31e551ab7f3a92c73b4d857b88cbf3b463c721a7
tech-kannnari <madpowertk@gmail.com>
2021-03-04 12:43:20 +0900

    不要となったconsole.logの削除

--- app/javascript/sales commission.js
+++ app/javascript/sales commission.js
@@ -3,16 +3,14 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-    console.log(addTaxDom);
+
 
     const addTaxDomValue = addTaxDom.innerHTML
     const salesProfit = document.getElementById("profit")
     salesProfit.innerHTML = (Math.floor(inputValue - addTaxDomValue));
-    console.log(salesProfit)
   })
 
 });
