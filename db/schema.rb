b802439c4b616b7ee05158f87626be3eb52dd10a
tech-kannnari <madpowertk@gmail.com>
2021-03-04 18:41:02 +0900

    データリセットの際の変更内容？

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_085759) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", default: "", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_085759) do
     t.string "last_kana", null: false
     t.string "kana", null: false
     t.date "birthday", null: false
-    t.string "reset_password_token", default: ""
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
