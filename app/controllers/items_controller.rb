75e594326e623d4e11a23bb5e9ee7c534262108d
tech-kannnari <madpowertk@gmail.com>
2021-03-06 12:30:42 +0900

    誤って消した記述の復旧

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,11 @@ class ItemsController < ApplicationController
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
 private
