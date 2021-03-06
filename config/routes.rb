66254815bf2f3cddb61f3925619237a0c2fce4e6
tech-kannnari <madpowertk@gmail.com>
2021-03-06 17:47:23 +0900

    不要な記述の削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # アクセスがあったら"items#index"へルーティングする
   root to: "items#index"
-  resources :items, only:[:new, :create, :show, :edit, :update, :destroy]
+  resources :items
 
 end
 
