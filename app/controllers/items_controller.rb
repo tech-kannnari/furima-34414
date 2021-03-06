61fa9c41da057873fce044446524ea067867ab09
tech-kannnari <madpowertk@gmail.com>
2021-03-06 17:43:45 +0900

    before_actionにdestroyの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_params, only:[:show, :edit, :update, :destroy]
   before_action :redirect_to_root, only:[:edit, :update, :destory]
 
