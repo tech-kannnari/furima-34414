e556becf0a4b57522eb99482fe113e39ecb209ba
tech-kannnari <madpowertk@gmail.com>
2021-03-09 18:22:43 +0900

    テーブルに保存をするた目の記述

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -4,9 +4,19 @@ class HistoriesController < ApplicationController
   end
 
   def new
+    @history = History.new
   end
 
   def create
+    @history = History.new(history_params)
+    @item = Item.find(params[:id])
+    if @history.save
+      redirect_to item_path(@item.id)
+    else
+      render :new
+    end
   end
+
   
+
 end
