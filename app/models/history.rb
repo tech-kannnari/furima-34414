3d0050c6aa0034f9da28d9d51e5591a49b9f6bdd
tech-kannnari <madpowertk@gmail.com>
2021-03-09 16:47:58 +0900

    アソシエーションの記述

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,2 +1,5 @@
 class History < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+  has_one    :buyer
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :histories
 
   # validates :email, uniqueness: true ← こちらはuserのmigrateファイル内にデフォルトでバリデイトされているので記述はいらない
   with_options presence: true do
