dcc68e68cf1f2ba3fabb1dd708b2577a21f495f7
tech-kannnari <madpowertk@gmail.com>
2021-03-10 14:53:16 +0900

    history_buyer内の修正

--- app/models/history_buyer.rb
+++ app/models/history_buyer.rb
@@ -1,6 +1,6 @@
 class HistoryBuyer
   include ActiveModel::Model
-  attr_accessor :postal_code, :delivery_area_id, :municipalities, :address, :building, :phone_number, :history
+  attr_accessor :postal_code, :delivery_area_id, :municipalities, :address, :building, :phone_number, :history, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -12,7 +12,7 @@ class HistoryBuyer
   validates :delivery_area_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    history = History.create(item: item, user_id: user_id)
+    history = History.create(item_id: item_id, user_id: user_id)
     Buyer.create(postal_code: postal_code, delivery_area_id: delivery_area_id, municipalities: municipalities, address: address, building: building, phone_number: phone_number, history: history)
   end
 end
