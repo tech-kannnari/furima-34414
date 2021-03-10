09eca863f4e92e04c25d1ecac5fbace32b9236c1
tech-kannnari <madpowertk@gmail.com>
2021-03-10 11:50:52 +0900

    ネスト化で変更になったパスの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # アクセスがあったら"items#index"へルーティングする
   root to: "items#index"
-  resources :items
-  resources :histories, only:[:index, :new, :create]
+  resources :items do
+    resources :histories, only:[:index, :new, :create]
+  end
 
 end
 
