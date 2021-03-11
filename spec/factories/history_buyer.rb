d9416a2d67d0fa063e480022cd747285ffb8fb63
tech-kannnari <madpowertk@gmail.com>
2021-03-11 12:16:25 +0900

    ファイル名の変更tokenの追加

--- spec/factories/history_buyer.rb
+++ spec/factories/history_buyer.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :history_buyer do
+    token            {"tok_abcdefghijk00000000000000000"}
     postal_code      {"000-0000"}
     delivery_area_id {2}
     municipalities   {"仙台市"}
